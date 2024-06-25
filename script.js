9a56fe8a31e6174dfd83b14519e370e5a6461260
Ingo (School Account) <0358381@schoolsnet.act.edu.au>
2024-06-25 13:18:05 +1000

    tweak values

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 particlesJS("particles-js", {
   particles: {
-    number: { value: 80, density: { enable: true, value_area: 800 } },
+    number: { value: 690, density: { enable: true, value_area: 420 } },
     color: { value: "#ffffff" },
     shape: {
       type: "circle",
